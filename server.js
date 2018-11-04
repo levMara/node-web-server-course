c7bb50853e456c1153b1874b8bb18ccd97105a34
לוי מ <levim770@gmail.com>
2018-11-04 14:38:08 +0200

    adding prjects page

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ app.get('/',(req, res) => {
   })
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs',{
+    pageTitle: 'Projects page'
+  })
+})
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About page',
